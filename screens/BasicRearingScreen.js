defc03f4163ff5cc7ac2698dfe4182dd032c89b3
Chanisaranest <73680459+Chanisaranest@users.noreply.github.com>
2022-11-19 17:31:47 +0700

    Update BasicRearingScreen.js

--- screens/BasicRearingScreen.js
+++ screens/BasicRearingScreen.js
@@ -1,13 +1,23 @@
-import React, { useEffect } from "react"; 
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect ,useState} from "react"; 
+import { StyleSheet, Text, View,TouchableWithoutFeedback ,ScrollView, Dimensions} from 'react-native';
+import CatData from "../app/CatData";
+import DogData from "../app/DogData";
 // import firebase from "firebase";
 
-const FindMyPetScreen = () => {
-  // useEffect({
-  //   const subjCollection = firebase.firestore().collection("mypets");
-  // }, [])
+
+const BasicLearningScreen = () => {
     return(
-        <Text>This is BasicRearingScreen</Text>
+        <View style={styles.container}>
+          <View style={{flexDirection:'row',marginTop:20, padding:20 ,backgroundColor:'rgba(27,27,51,1)', borderRadius:20}} >
+          <Text style={{fontSize:18,color:'white'}}>วิธีการเลี้ยงดูโดยทั่วไป</Text>
+          </View>
+        <ScrollView horizontal pagingEnabled showsHorizontalScrollIndicator={false}>
+          <DogData />
+          <CatData />
+          
+        </ScrollView>
+        
+        </View>
     );
 }
 
@@ -20,6 +30,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
 
-export default FindMyPetScreen;
+export default BasicLearningScreen;
