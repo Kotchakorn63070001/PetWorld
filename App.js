cce9c7314ab4f23c8ec4f089ebc75befbb4de6f3
it63070029 <73680368+it63070029@users.noreply.github.com>
2022-12-19 20:23:32 +0700

    Update App.js

--- App.js
+++ App.js
@@ -24,14 +24,5 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: '#fff',
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
-  // root: {
-  //   flex: 1,
-  //   backgroundColor: '#f9fbfc',
-  // }
+
 });
